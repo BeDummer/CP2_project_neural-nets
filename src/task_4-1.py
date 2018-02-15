7e821b1cbc2f0e8f76a2b92d8f2c589bf085880a
BeDummer <benjamin.dummer@googlemail.com>
2018-02-15 17:57:49 +0100

    corrections - not properly working right now

--- src/task_4-1.py
+++ src/task_4-1.py
@@ -4,37 +4,42 @@
 import numpy as np
 import hopfield_func as hf
 
-n = 100 #number of neurons
-p = np.array([10, 20, 30]) #number of pictures
-conv_error = 0.1 #definition, when a fixpoint is reached
-max_iter = 1000; #maximum number of iterations (synchronous update)
+N = 100 #number of neurons
+P = np.array([10, 20, 30]) #number of pictures
+CONV_ERROR = 0.1 #definition, when a fixpoint is reached
+MAX_ITER = 1000; #maximum number of iterations (synchronous update)
 
-for i_p in range(len(p)):
+for i_p in range(len(P)):
     #loop over different numbers of pictures
-    picts = hf.rand_picts(n, p[i_p]) 
-    w = hf.set_synapse(picts, p[i_p], n)
-    errors = np.zeros((p[i_p],2))
-    iters = np.zeros((p[i_p],1))
+    picts = hf.rand_picts(N, P[i_p])
+    print(picts)
+    w = hf.set_synapse(picts, P[i_p], N)
+    errors = np.zeros((P[i_p],2))
+    iters = np.zeros((P[i_p],1))
+    print(w)
     
-    for pic in range(p[i_p]):
+    for pic in range(P[i_p]):
         #loop over pictures
         s1 = picts[:, pic]
-        s2 = update_async(s1, w)
-        d_old = 0
-        d = hamming(s1, s2)
+        s1 = hf.rand_signal(N)
+        #print(s1)
+        s2 = hf.update_async(s1, w)
+        #print(s2)
+        d_old = N+1
+        d = hf.hamming(s1, s2)
         errors[pic, 0] = d
         count = 0
         
-        while np.absolute(d_old - d) > conv_error:
+        while np.absolute(d_old - d) > CONV_ERROR:
             #convergence loop to reach fixpoint
-            count = count + 1
+            count += 1
             d_old = d
             s1 = s2
-            s2 = update_async(s1, w)
-            d = hamming(s1, s2)
+            s2 = hf.update_async(s1, w)
+            d = hf.hamming(s1, s2)
         
         errors[pic, 1] = d
         iters[pic] = count
     
-    errors
-    iters
+    print(errors)
+    print(iters)
