43a3f5781baf3536cf42524882cb2a6943395dcb
BeDummer <benjamin.dummer@googlemail.com>
2018-02-20 00:26:52 +0100

    now working + task 4.2

--- src/hopfield_func.py
+++ src/hopfield_func.py
@@ -19,10 +19,10 @@ def set_synapse(picts, p, n):
     """Sets synapses for given pictures after Hebbs rule."""
     w = np.zeros((n, n))
     for i in range(p):
-        w = w + np.outer(picts[:, i], picts[:, i])
+        w = np.add(w, np.outer(picts[:, i], picts[:, i]))
     for i in range(n):
-        w[i, i] = 0
-    w = w / n
+        w[i, i] = 0.
+    w = np.divide(w,n)
     return w
 
 def rand_picts(n, p):
@@ -30,20 +30,35 @@ def rand_picts(n, p):
     picts = np.sign(np.random.rand(n, p) - 0.5)
     return picts
 
-def update_sync(s1, w):
-    """Updates a signal s synchronously with synapses w."""
-    s2 = np.sign(w.dot(s))
-    return s2
-
-def update_async(s1, w):
-    """Updates a signal s asynchronously with synapses w."""
-    s2 = s1
-    n = len(s1)
-    for i in range(n):
-        s2[i] = np.sign(np.inner(w[i, :], s2))
-    return s2
+def update(s1, w, async=True):
+    """Updates a signal s asynchronously or synchronously with synapses w."""
+    if async:
+        s2 = s1.copy()
+        n = len(s1)
+        for i in range(n):
+            cache = np.sign(np.inner(w[i, :], s2))
+            if (cache == 0):
+                cache = -1
+            s2[i] = cache
+        return s2
+    else:
+        s2 = np.sign(w.dot(s1))
+        return s2
 
 def rand_signal(n):
     """Generates a random signal of length n."""
     s = np.sign(np.random.rand(n) - 0.5)
     return s
+    
+def is_pic(s, picts, error=1, fancy_hamming=False):
+    """Checks, if signal s is (almost) equal to a given picture
+    ... and returns the number of the picture (or -1 if false)"""
+    p = picts.shape[(picts.ndim - 1)]
+    for pic in range(p):
+        if fancy_hamming:
+            if (hamming_sym(s, picts[:, pic]) < error):
+                return pic
+        else:
+            if (hamming(s, picts[:, pic]) < error):
+                return pic
+    return (-1)
--- src/task_4-2.py
+++ src/task_4-2.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
-# main programm for simulation of Hopfield model: task 4.1
+# main programm for simulation of Hopfield model: task 4.2
 
 import numpy as np
 import hopfield_func as hf
 
 N = 100 #number of neurons
 P = np.array([10, 20, 30]) #number of pictures
-MAX_ITER = 100; #maximum number of iterations (synchronous update)
+CONV_ERROR = 5 #definition, when a picture is reached
+MAX_ITER = 100 #maximum number of iterations (synchronous update)
+CONFIGS = 10 #number of random configurations
 UPDATE_MODE = True #True = asynchronous update, False = synchronous update
 np.random.seed()
 
@@ -14,15 +16,14 @@ for i_p in range(len(P)):
     #loop over different numbers of pictures
     picts = hf.rand_picts(N, P[i_p])
     w = hf.set_synapse(picts, P[i_p], N)
-    errors = np.zeros((P[i_p],2))
-    iters = np.zeros((P[i_p],1))
-    ispic = np.zeros((P[i_p],1))
+    errors = np.zeros((CONFIGS,1))
+    iters = np.zeros((CONFIGS,1))
+    ispic = np.zeros((CONFIGS,1))
     
-    for pic in range(P[i_p]):
+    for pic in range(CONFIGS):
         #loop over pictures
-        s1 = picts[:, pic].copy()
+        s1 = hf.rand_signal(N)
         s2 = hf.update(s1, w, UPDATE_MODE)
-        errors[pic, 0] = hf.hamming(s1, s2) / N
         count = 0        
 
         while (count < MAX_ITER) and not (np.array_equal(s1, s2)):
@@ -31,9 +32,12 @@ for i_p in range(len(P)):
             s1 = s2.copy()
             s2 = hf.update(s1, w, UPDATE_MODE)
         
-        errors[pic, 1] = hf.hamming(picts[:, pic], s2) / N
         iters[pic] = count
-        ispic[pic] = hf.is_pic(s2, picts)
+        ispic[pic] = hf.is_pic(s2, picts, CONV_ERROR, True)
+        if (ispic[pic] != -1):
+            errors[pic] = hf.hamming_sym(picts[:, int(ispic[pic])], s2) / N
+        else:
+            errors[pic] = -1
     
     print(errors)
     print(iters)
