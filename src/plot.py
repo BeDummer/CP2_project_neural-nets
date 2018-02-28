a4bbbb6079fe408773207eafd26a5667840aa270
BeDummer <benjamin.dummer@googlemail.com>
2018-02-28 13:58:55 +0100

    result conclusion + new big run

--- src/plot.py
+++ src/plot.py
@@ -4,7 +4,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-FILENAME="../results/task_4-2_run_N-100_CONF-1000_ERR-5_RUNS-100_ASYNC.txt"
+FILENAME="../results/task_4-2_run_N-100_CONF-1000_ERR-5_RUNS-1000_ASYNC_complete.txt"
 
 with open(FILENAME) as f:
     f.readline()
@@ -16,16 +16,22 @@ for line in data:
     array = np.vstack((array, array_tmp))
 
 array = array[1:, :]
-beta = np.arange(11)
 sp_state = array[:, 3]
+length = int(len(sp_state) / 9)
+beta = np.arange(length)
 count = 0
-P10 = np.zeros(11)
-P20 = np.zeros(11)
-P30 = np.zeros(11)
-for i in range(7, len(sp_state), 9):
-    P10[count] = sp_state[i-1]
+P10 = np.zeros(length)
+P20 = np.zeros(length)
+P30 = np.zeros(length)
+#for i in range(7, len(sp_state), 9):
+#    P10[count] = sp_state[i-1]
+#    P20[count] = sp_state[i]
+#    P30[count] = sp_state[i+1]
+#    count += 1
+for i in range(3, len(sp_state), 9):
+    P10[count] = sp_state[i-3]
     P20[count] = sp_state[i]
-    P30[count] = sp_state[i+1]
+    P30[count] = sp_state[i+3]
     count += 1
     
 plt.plot(beta, P10, 'r-*', beta, P20, 'g-*', beta, P30, 'b-*')
--- src/task_4-2_big-run.py
+++ src/task_4-2_big-run.py
@@ -12,46 +12,46 @@ CONFIGS = 1000 #number of random configurations
 RUNS = 1000
 UPDATE_MODE = True #True = asynchronous update, False = synchronous update
 BETA = 4 #False = no finite temperature implemented, else finite temp. with BETA
-BETA_ITER = 5 #number of iterations with finite temperature
-FILENAME = "../results/task_4-2_run_N-{0}_CONF-{1}_ERR-{2}_RUNS-{3}_BETA_14-15_ASYNC.txt".format(N, CONFIGS, CONV_ERROR, RUNS)
+BETA_ITER = 30 #number of iterations with finite temperature
+FILENAME = "../results/task_4-2_run_N-{0}_CONF-{1}_ERR-{2}_RUNS-{3}_BEIT-{4}_BETA_0-4_ASYNC.txt".format(N, CONFIGS, CONV_ERROR, RUNS, BETA_ITER)
 np.random.seed()
 
 with open(FILENAME,"w") as f:
     f.write("beta beta_iter P sp_state\n")
-for BETA in range(14, 16):
+for BETA in range(5):
     #loop over different betas
-    for BETA_ITER in range(5, 16, 5):
+#    for BETA_ITER in range(5, 16, 5):
         #loop over different numbers of iterations with finite temp.
-        for i_p in range(len(P)):
-            #loop over different numbers of pictures
-            sp_state = 0
-            for run in range(RUNS):
-                picts = hf.rand_picts(N, P[i_p])
-                w = hf.set_synapse(picts, P[i_p], N)
-                
-                for pic in range(CONFIGS):
-                    #loop over pictures
-                    beta = BETA
-                    s1 = hf.rand_signal(N)
+    for i_p in range(len(P)):
+        #loop over different numbers of pictures
+        sp_state = 0
+        for run in range(RUNS):
+            picts = hf.rand_picts(N, P[i_p])
+            w = hf.set_synapse(picts, P[i_p], N)
+            
+            for pic in range(CONFIGS):
+                #loop over pictures
+                beta = BETA
+                s1 = hf.rand_signal(N)
+                s2 = hf.update(s1, w, UPDATE_MODE, beta)
+                count = 1
+                while beta:
+                    s1 = s2.copy()
                     s2 = hf.update(s1, w, UPDATE_MODE, beta)
-                    count = 1
-                    while beta:
-                        s1 = s2.copy()
-                        s2 = hf.update(s1, w, UPDATE_MODE, beta)
-                        count += 1
-                        if (count == BETA_ITER):
-                            beta=False
+                    count += 1
+                    if (count == BETA_ITER):
+                        beta=False
 
-                    while (count < MAX_ITER) and not (np.array_equal(s1, s2)):
-                        #convergence loop to reach fixpoint
-                        count += 1
-                        s1 = s2.copy()
-                        s2 = hf.update(s1, w, UPDATE_MODE, beta)
-                    
-                    ispic = hf.is_pic(s2, picts, CONV_ERROR, True)
-                    if (ispic == -1):
-                        sp_state += 1
-                    
-            with open(FILENAME,"a") as f:
-                f.write("{0} {1} {2} {3}\n".format(BETA, BETA_ITER, P[i_p], sp_state/CONFIGS/RUNS))
-            print("{0} {1} {2} {3}".format(BETA, BETA_ITER, P[i_p], sp_state/CONFIGS/RUNS))
+                while (count < MAX_ITER) and not (np.array_equal(s1, s2)):
+                    #convergence loop to reach fixpoint
+                    count += 1
+                    s1 = s2.copy()
+                    s2 = hf.update(s1, w, UPDATE_MODE, beta)
+                
+                ispic = hf.is_pic(s2, picts, CONV_ERROR, True)
+                if (ispic == -1):
+                    sp_state += 1
+                
+        with open(FILENAME,"a") as f:
+            f.write("{0} {1} {2} {3}\n".format(BETA, BETA_ITER, P[i_p], sp_state/CONFIGS/RUNS))
+        print("{0} {1} {2} {3}".format(BETA, BETA_ITER, P[i_p], sp_state/CONFIGS/RUNS))
