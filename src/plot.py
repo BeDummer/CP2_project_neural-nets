8552ec000104564c078cde08829787d6beaceeb4
BeDummer <benjamin.dummer@googlemail.com>
2018-03-23 21:18:54 +0100

    ok, now its the last run

--- src/plot.py
+++ src/plot.py
@@ -17,28 +17,34 @@ for line in data:
 
 array = array[1:, :]
 sp_state = array[:, 3]
-length = int(len(sp_state) / 12)
+length = int(len(sp_state) / 13)
 beta = np.arange(length)
 count = 0
 #P10 = np.zeros(length)
 #P20 = np.zeros(length)
 #P30 = np.zeros(length)
-P10a = np.zeros(length)
-P10b = np.zeros(length)
-P10c = np.zeros(length)
-P10d = np.zeros(length)
+P10a = np.zeros(length-1)
+P10b = np.zeros(length-1)
+P10c = np.zeros(length-1)
+P10d = np.zeros(length-1)
+P10e = np.zeros(length-1)
 #for i in range(7, len(sp_state), 9):
 #    P10[count] = sp_state[i-1]
 #    P20[count] = sp_state[i]
 #    P30[count] = sp_state[i+1]
 #    count += 1
-for i in range(5, len(sp_state), 12):
-    P10a[count] = sp_state[i-3]
-    P10b[count] = sp_state[i]
-    P10c[count] = sp_state[i+3]
-    P10d[count] = sp_state[i+6]
-    count += 1
+for i in range(3, len(sp_state), 13):
+    if (i == 3):
+        P10inf = (sp_state[i-3] + sp_state[i] + sp_state[i+3] + sp_state[i+6] + sp_state[i+9]) / 5
+    else:
+        P10a[count] = sp_state[i-3]
+        P10b[count] = sp_state[i]
+        P10c[count] = sp_state[i+3]
+        P10d[count] = sp_state[i+6]
+        P10e[count] = sp_state[i+9]
+        count += 1
     
 #plt.plot(beta, P10, 'r-*', beta, P20, 'g-*', beta, P30, 'b-*')
-plt.plot(beta, P10a, 'r-*', beta, P10b, 'g-*', beta, P10c, 'b-*', beta, P10d, 'k-*')
+plt.plot(beta[1:], P10a, 'r-*', beta[1:], P10b, 'g-*', beta[1:], P10c, 'b-*', beta[1:], P10d, 'm-*', beta[1:], P10e, 'c-*', [0. , length-1], [P10inf, P10inf], 'k--')
+plt.xticks(beta)
 plt.show()
